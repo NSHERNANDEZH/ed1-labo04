69e032cfe230e807cc7380ed51ef92332e780083
Sam <nsamuel.gt@gmail.com>
2025-05-26 11:25:07 -0600

    Push.1

---
+++ src/main/java/ed/lab/ed1labo04/model/CreateCartRequest.java
@@ -0,0 +1,19 @@
+package ed.lab.ed1labo04.model;
+
+import ed.lab.ed1labo04.entity.CartItem;
+
+import java.util.List;
+
+public class CreateCartRequest {
+
+    private List<CartItem> cartItems;
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+}
